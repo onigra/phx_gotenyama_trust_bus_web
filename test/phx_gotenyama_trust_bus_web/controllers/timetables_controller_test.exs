ef524eae97507c1410f81811edee4dfa9187addd
onigra <3280467rec@gmail.com>
2019-10-13 15:29:14 +0900

    shinagawaの時刻表出すcontrollerのテストを追加

--- test/phx_gotenyama_trust_bus_web/controllers/timetables_controller_test.exs
+++ test/phx_gotenyama_trust_bus_web/controllers/timetables_controller_test.exs
@@ -37,4 +37,71 @@ defmodule GotenyamaTrustBusWeb.TimeTablesControllerTest do
     # then
     assert expected == actual
   end
+
+  test "shinagawa/2", %{conn: conn} do
+    # setup
+    expected = %{
+      "data" => %{
+        "timetables" => %{
+          "shinagawa" => %{
+            "weekday" => key_to_string(Timetable.Shinagawa.weekday()),
+            "holiday" => key_to_string(Timetable.Shinagawa.holiday())
+          }
+        }
+      }
+    }
+
+    # when
+    actual =
+      conn
+      |> get(Routes.timetables_path(conn, :shinagawa))
+      |> json_response(200)
+
+    # then
+    assert expected == actual
+  end
+
+  test "shinagawa_weekday/2", %{conn: conn} do
+    # setup
+    expected = %{
+      "data" => %{
+        "timetables" => %{
+          "shinagawa" => %{
+            "weekday" => key_to_string(Timetable.Shinagawa.weekday())
+          }
+        }
+      }
+    }
+
+    # when
+    actual =
+      conn
+      |> get(Routes.timetables_path(conn, :shinagawa_weekday))
+      |> json_response(200)
+
+    # then
+    assert expected == actual
+  end
+
+  test "shinagawa_holiday/2", %{conn: conn} do
+    # setup
+    expected = %{
+      "data" => %{
+        "timetables" => %{
+          "shinagawa" => %{
+            "holiday" => key_to_string(Timetable.Shinagawa.holiday())
+          }
+        }
+      }
+    }
+
+    # when
+    actual =
+      conn
+      |> get(Routes.timetables_path(conn, :shinagawa_holiday))
+      |> json_response(200)
+
+    # then
+    assert expected == actual
+  end
 end
