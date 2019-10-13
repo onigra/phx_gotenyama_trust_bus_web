d8579ed317194b81f876586f64328ea751ca2ad3
onigra <3280467rec@gmail.com>
2019-10-14 00:56:32 +0900

    現在時刻見て、次に来るバスの時刻を返す

--- lib/phx_gotenyama_trust_bus_web/controllers/next_controller.ex
+++ lib/phx_gotenyama_trust_bus_web/controllers/next_controller.ex
@@ -1,8 +1,35 @@
 defmodule GotenyamaTrustBusWeb.NextController do
   use GotenyamaTrustBusWeb, :controller
 
-  def index(conn, _params) do
-    next_time = "12:05"
+  def index(conn, params) do
+    tt = timetable(params["from"])
+    now = Timex.local()
+    today = to_date(now)
+
+    t =
+      if HolidayJp.holiday?(today) do
+        tt.holiday
+      else
+        tt.weekday
+      end
+
+    current = t[now.hour]
+    list = Enum.filter(current, fn x -> x > now.minute end)
+
+    next_time = "#{now.hour}:#{hd(list)}"
     render(conn, "index.json", %{next_time: next_time})
+  end
+
+  defp timetable(from) do
+    case from do
+      "shinagawa" -> Timetables.Shinagawa
+      "gotenyama" -> Timetables.Gotenyama
+      _ -> raise ArgumentError
+    end
+  end
+
+  defp to_date(datetime) do
+    today = Date.new(datetime.year, datetime.month, datetime.day)
+    elem(today, 1)
   end
 end
