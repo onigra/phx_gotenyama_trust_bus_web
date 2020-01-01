04a1286ddc078c0a9c77b041b75fdbb44a49c384
onigra <3280467rec@gmail.com>
2020-01-01 23:32:08 +0900

    今日の時刻表を探すモジュールを切り出した

--- lib/phx_gotenyama_trust_bus_web/controllers/next_controller.ex
+++ lib/phx_gotenyama_trust_bus_web/controllers/next_controller.ex
@@ -2,31 +2,9 @@ defmodule GotenyamaTrustBusWeb.NextController do
   use GotenyamaTrustBusWeb, :controller
 
   def index(conn, params) do
-    tt = timetable(params["from"])
     now = Timex.local()
-    today = to_date(now)
-
-    t =
-      if HolidayJp.holiday?(today) do
-        tt.holiday
-      else
-        tt.weekday
-      end
-
-    next_time = NextBus.lookup(t, now)
+    timetable = TodaysTimetable.lookup(params["from"], now)
+    next_time = NextBus.lookup(timetable, now)
     render(conn, "index.json", %{next_time: next_time})
-  end
-
-  defp timetable(from) do
-    case from do
-      "shinagawa" -> Timetables.Shinagawa
-      "gotenyama" -> Timetables.Gotenyama
-      _ -> raise ArgumentError
-    end
-  end
-
-  defp to_date(datetime) do
-    today = Date.new(datetime.year, datetime.month, datetime.day)
-    elem(today, 1)
   end
 end
---
+++ lib/phx_gotenyama_trust_bus_web/models/todays_timetable.ex
@@ -0,0 +1,26 @@
+defmodule TodaysTimetable do
+  @spec lookup(String.t(), DateTime.t()) :: map()
+  def lookup(from, now) do
+    tt = timetable(from)
+    today = to_date(now)
+
+    if HolidayJp.holiday?(today) do
+      tt.holiday
+    else
+      tt.weekday
+    end
+  end
+
+  defp timetable(from) do
+    case from do
+      "shinagawa" -> Timetables.Shinagawa
+      "gotenyama" -> Timetables.Gotenyama
+      _ -> raise ArgumentError
+    end
+  end
+
+  defp to_date(datetime) do
+    today = Date.new(datetime.year, datetime.month, datetime.day)
+    elem(today, 1)
+  end
+end
