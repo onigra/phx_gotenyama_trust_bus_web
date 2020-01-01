f96865bc5046b05e83b814cfc78c725741fc74c4
onigra <3280467rec@gmail.com>
2020-01-02 01:09:32 +0900

    Timex.local()を叩いてるから念のため最初に変数に入れる

--- lib/phx_gotenyama_trust_bus_web/controllers/next_controller.ex
+++ lib/phx_gotenyama_trust_bus_web/controllers/next_controller.ex
@@ -2,12 +2,15 @@ defmodule GotenyamaTrustBusWeb.NextController do
   use GotenyamaTrustBusWeb, :controller
 
   def index(conn, params) do
-    timetable = TodaysTimetable.lookup(params["from"], current_timestamp(params["now"]))
-    next_time = NextBus.lookup(timetable, current_timestamp(params["now"]))
+    from = params["from"]
+    current_timestamp = current_datetime(params["now"])
+
+    timetable = TodaysTimetable.lookup(from, current_timestamp)
+    next_time = NextBus.lookup(timetable, current_timestamp)
     render(conn, "index.json", %{next_time: next_time})
   end
 
-  defp current_timestamp(now) do
+  defp current_datetime(now) do
     if now == nil do
       Timex.local()
     else
