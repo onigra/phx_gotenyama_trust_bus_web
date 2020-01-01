6e5ef30138766014ebffee3942dac5cce5dcb8ff
onigra <3280467rec@gmail.com>
2020-01-02 01:02:58 +0900

    テスト用に基準にするタイムスタンプを引数で指定できるようにした

--- lib/phx_gotenyama_trust_bus_web/controllers/next_controller.ex
+++ lib/phx_gotenyama_trust_bus_web/controllers/next_controller.ex
@@ -2,9 +2,16 @@ defmodule GotenyamaTrustBusWeb.NextController do
   use GotenyamaTrustBusWeb, :controller
 
   def index(conn, params) do
-    now = Timex.local()
-    timetable = TodaysTimetable.lookup(params["from"], now)
-    next_time = NextBus.lookup(timetable, now)
+    timetable = TodaysTimetable.lookup(params["from"], current_timestamp(params["now"]))
+    next_time = NextBus.lookup(timetable, current_timestamp(params["now"]))
     render(conn, "index.json", %{next_time: next_time})
+  end
+
+  defp current_timestamp(now) do
+    if now == nil do
+      Timex.local()
+    else
+      Timex.parse!(now, "%FT%T%:z", :strftime)
+    end
   end
 end
