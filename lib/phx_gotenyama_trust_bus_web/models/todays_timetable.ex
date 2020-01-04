f1d5fe551dbd18acc8ee55cd5cbeffb86fde5d4d
onigra <3280467rec@gmail.com>
2020-01-04 23:06:14 +0900

    今日のタイムテーブル取得もパターンマッチで

--- lib/phx_gotenyama_trust_bus_web/models/todays_timetable.ex
+++ lib/phx_gotenyama_trust_bus_web/models/todays_timetable.ex
@@ -1,26 +1,23 @@
 defmodule TodaysTimetable do
   @spec lookup(String.t(), DateTime.t()) :: map()
   def lookup(from, now) do
-    tt = timetable(from)
-    today = to_date(now)
-
-    if HolidayJp.holiday?(today) do
-      tt.holiday
-    else
-      tt.weekday
-    end
-  end
-
-  defp timetable(from) do
-    case from do
-      "shinagawa" -> Timetables.Shinagawa
-      "gotenyama" -> Timetables.Gotenyama
-      _ -> raise ArgumentError
-    end
+    timetable(from, HolidayJp.holiday?(to_date(now)))
   end
 
   @spec to_date(DateTime.t()) :: Date.t()
   defp to_date(dt) do
     with {:ok, date} <- Date.new(dt.year, dt.month, dt.day), do: date
   end
+
+  defp timetable(from, holiday?) when from == "shinagawa" and holiday? == true,
+    do: Timetables.Shinagawa.holiday()
+
+  defp timetable(from, holiday?) when from == "shinagawa" and holiday? == false,
+    do: Timetables.Shinagawa.weekday()
+
+  defp timetable(from, holiday?) when from == "gotenyama" and holiday? == true,
+    do: Timetables.Gotenyama.holiday()
+
+  defp timetable(from, holiday?) when from == "gotenyama" and holiday? == false,
+    do: Timetables.Gotenyama.weekday()
 end
